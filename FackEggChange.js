ec03e7cbc47359f5e4fda3c3c7f8a3c3584e6bf5
CnData07 <data07@126.com>
2022-09-04 20:07:37 +0800

    Initial commit

---
+++ FackEggChange.js
@@ -0,0 +1,13 @@
+//LiteLoaderScript Dev Helper
+/// <reference path="G:/Minecraft/LXL/JS_Edit/HelperLib-master/src/index.d.ts"/> 
+
+const _VER = '1.0.1';
+
+mc.listen('onUseItemOn', (pl,item,block)=>{
+        if (pl.gameMode != 1 && item.type == "minecraft:spawn_egg" && block.type == "minecraft:mob_spawner"){
+            return false;
+        }
+    }
+)
+
+colorLog("yellow", "禁止刷怪蛋改变刷怪笼插件加载完成!");
